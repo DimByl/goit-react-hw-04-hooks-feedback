c57a006eb65581ab913a0d6241b631ad6ed4b8ad
Dmytro Bykanov <75586266+DimByl@users.noreply.github.com>
2021-09-30 02:19:18 +0300

    up2

--- src/App.js
+++ src/App.js
@@ -1,66 +1,63 @@
-import { Component } from "react";
+import { useState } from "react";
 import FeedbackOptions from "./components/FeedbackOptions/FeedbackOptions";
 import Statistics from "./components/Statistics/Statistics";
 import Section from "./components/Section/Section";
 import Notification from "./components/Notification/Notification";
 import Container from "./components/Container/Container";
 
-class App extends Component {
-  state = {
+const App = () => {
+  const [state, setState] = useState({
     good: 0,
     neutral: 0,
     bad: 0,
-  };
-  countLeftFeedbackHandler = (value) => {
-    this.setState((prevState) => ({
+  });
+
+  const { good, neutral, bad } = state;
+
+  const countLeftFeedbackHandler = (value) =>
+    setState((prevState) => ({
+      ...state,
       [value]: prevState[value] + 1,
     }));
-  };
 
-  countTotalFeedback = (arrayOfValues) =>
+  const countTotalFeedback = (arrayOfValues) =>
     arrayOfValues.reduce((acc, value) => acc + value, 0);
 
-  countPositiveFeedbackPercentage = (good, total) =>
+  const countPositiveFeedbackPercentage = () =>
     Math.round((good / total) * 100);
 
-  render() {
-    const { good, neutral, bad } = this.state;
+  const keys = Object.keys(state);
+  const values = Object.values(state);
+  const total = countTotalFeedback(values);
+  const positivePercentage = countPositiveFeedbackPercentage();
 
-    const keys = Object.keys(this.state);
-    const values = Object.values(this.state);
-    const total = this.countTotalFeedback(values);
-    const positivePercentage = this.countPositiveFeedbackPercentage(
-      good,
-      total
-    );
+  return (
+    <div className="App">
+      <h1>React hw 2 - Feedback </h1>
+      <Container>
+        <Section title="Please leave feedback">
+          <FeedbackOptions
+            options={keys}
+            onLeaveFeedback={countLeftFeedbackHandler}
+          />
+        </Section>
 
-    return (
-      <div className="App">
-        <h1>React hw 2 - Feedback </h1>
-        <Container>
-          <Section title="Please leave feedback">
-            <FeedbackOptions
-              options={keys}
-              onLeaveFeedback={this.countLeftFeedbackHandler}
+        <Section title="Statistics">
+          {total > 0 ? (
+            <Statistics
+              good={good}
+              neutral={neutral}
+              bad={bad}
+              total={total}
+              positivePercentage={positivePercentage}
             />
-          </Section>
+          ) : (
+            <Notification message="No feedback given" />
+          )}
+        </Section>
+      </Container>
+    </div>
+  );
+};
 
-          <Section title="Statistics">
-            {total > 0 ? (
-              <Statistics
-                good={good}
-                neutral={neutral}
-                bad={bad}
-                total={total}
-                positivePercentage={positivePercentage}
-              />
-            ) : (
-              <Notification message="No feedback given" />
-            )}
-          </Section>
-        </Container>
-      </div>
-    );
-  }
-}
 export default App;
